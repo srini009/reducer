1652a06843f5e8ecea10a2446121bc871192e91c
Srinivasan Ramesh <ramesh.srinivasan09@gmail.com>
2021-04-12 19:04:14 -0400

    Add some more changes

--- src/provider.c
+++ src/provider.c
@@ -190,7 +190,7 @@ static void reducer_metric_reduce_ult(hg_handle_t h)
     //    fprintf(stderr, "Received key with size: %d\n", ((double *)vals[i])[0]);
     double val = 0;
     size_t val_size = sizeof(double);
-    fprintf(stderr, "At server: trying to reduce metric with key: %s and aggid: %u\n", predix, in.agg_id);
+    fprintf(stderr, "At server: trying to reduce metric with key: %s and aggid: %u\n", prefix, in.agg_id);
     ret = sdskv_get(provider->aggphs[in.agg_id], provider->aggdbids[in.agg_id], (const void*)prefix, sizeof(prefix), (void*)&val, &val_size); 
     assert(ret == SDSKV_SUCCESS);
     if(!val && (in.op == REDUCER_REDUCTION_OP_MAX))
