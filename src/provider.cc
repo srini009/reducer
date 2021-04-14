5baa0aa16a8f8fbb88270e532ce7166c137cadfa
Srinivasan Ramesh <ramesh.srinivasan09@gmail.com>
2021-04-14 17:37:05 -0400

    Add some changes

--- src/provider.cc
+++ src/provider.cc
@@ -260,8 +260,8 @@ static void reducer_metric_reduce_ult(hg_handle_t h)
                 max = (max < val_doubles[i][j] ? val_doubles[i][j] : max);
             }
             metric_name += "MAX";
+    	    ret = symbiomon_metric_create(in.ns, metric_name.c_str(), SYMBIOMON_TYPE_GAUGE, metric_name.c_str(), taglist, &m, provider->metric_provider);
             if(!ret) trigger_metric_file_write = true;
-    	    symbiomon_metric_create(in.ns, metric_name.c_str(), SYMBIOMON_TYPE_GAUGE, metric_name.c_str(), taglist, &m, provider->metric_provider);
             symbiomon_metric_update(m, max);
             break;
         }
@@ -274,8 +274,8 @@ static void reducer_metric_reduce_ult(hg_handle_t h)
             }
             avg = sum/(double)max_keys;
             metric_name += "AVG";
+    	    ret = symbiomon_metric_create(in.ns, metric_name.c_str(), SYMBIOMON_TYPE_GAUGE, metric_name.c_str(), taglist, &m, provider->metric_provider);
             if(!ret) trigger_metric_file_write = true;
-    	    symbiomon_metric_create(in.ns, metric_name.c_str(), SYMBIOMON_TYPE_GAUGE, metric_name.c_str(), taglist, &m, provider->metric_provider);
             symbiomon_metric_update(m, avg);
             break;
         }
@@ -301,7 +301,7 @@ static void reducer_metric_reduce_ult(hg_handle_t h)
                 sd += pow(flattened_data[i] - avg, 2);
 
             metric_name += "ANOMALY";
-    	    symbiomon_metric_create(in.ns, metric_name.c_str(), SYMBIOMON_TYPE_GAUGE, metric_name.c_str(), taglist, &m, provider->metric_provider);
+    	    ret = symbiomon_metric_create(in.ns, metric_name.c_str(), SYMBIOMON_TYPE_GAUGE, metric_name.c_str(), taglist, &m, provider->metric_provider);
             if(!ret) trigger_metric_file_write = true;
 	    for(unsigned int i=0; i < current_index; i++) {
                 if ((flattened_data[i] < avg-3*sd) || (flattened_data[i] > avg+3*sd)) {
