8d29147d251d1a9f9b65b406ec0163d8eab2d178
Srinivasan Ramesh <ramesh.srinivasan09@gmail.com>
2021-04-15 15:53:19 -0400

    Add threads to reducer

--- bin/server.c
+++ bin/server.c
@@ -20,7 +20,7 @@ int main(int argc, char** argv)
 {
     (void)argc;
     (void)argv;
-    margo_instance_id mid = margo_init("ofi+verbs://", MARGO_SERVER_MODE, 0, 0);
+    margo_instance_id mid = margo_init("ofi+verbs://", MARGO_SERVER_MODE, 1, 2);
     assert(mid);
 
     hg_addr_t my_address;
