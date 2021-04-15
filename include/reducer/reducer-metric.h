6b3f92465d6c69cf40c4f93878a79d5f12b22796
Srinivasan Ramesh <ramesh.srinivasan09@gmail.com>
2021-04-15 15:30:21 -0400

    Add working global aggregation

--- include/reducer/reducer-metric.h
+++ include/reducer/reducer-metric.h
@@ -19,7 +19,7 @@ typedef struct reducer_provider* reducer_provider_t;
 #define REDUCER_METRIC_HANDLE_NULL ((reducer_metric_handle_t)NULL)
 
 /* APIs for remote clients to request for performance data reduction */
-reducer_return_t reducer_metric_reduce(const char *ns, const char *name, const char *key_start, uint32_t agg_id, reducer_metric_reduction_op_t op, reducer_metric_handle_t handle, size_t num_vals, size_t cohort_size);
+reducer_return_t reducer_metric_reduce(const char *ns, const char *name, const char *key_start, uint32_t agg_id, reducer_metric_reduction_op_t op, reducer_metric_handle_t handle, size_t cohort_size);
 reducer_return_t reducer_metric_handle_create(reducer_client_t client, hg_addr_t addr, uint16_t provider_id, reducer_metric_handle_t* handle);
 reducer_return_t reducer_metric_handle_ref_incr(reducer_metric_handle_t handle);
 reducer_return_t reducer_metric_handle_release(reducer_metric_handle_t handle);
