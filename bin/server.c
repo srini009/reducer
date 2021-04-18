1f0e38155bb9e900194f1e9c967e25bab4648f2a
Srinivasan Ramesh <sramesh@cs.uoregon.edu>
2021-04-18 21:21:00 +0000

    Add GNI protocol

--- bin/server.c
+++ bin/server.c
@@ -20,7 +20,7 @@ int main(int argc, char** argv)
 {
     (void)argc;
     (void)argv;
-    margo_instance_id mid = margo_init("ofi+verbs://", MARGO_SERVER_MODE, 1, 2);
+    margo_instance_id mid = margo_init("ofi+gni://", MARGO_SERVER_MODE, 1, 2);
     assert(mid);
 
     hg_addr_t my_address;
