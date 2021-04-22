a9b1f1e51a2b49901f1a4f43a62ec9b9f4837794
Srinivasan Ramesh <ramesh.srinivasan09@gmail.com>
2021-04-22 02:51:11 -0400

    Add some printfs

--- src/provider.cc
+++ src/provider.cc
@@ -260,6 +260,7 @@ static void reducer_metric_reduce_ult(hg_handle_t h)
             symbiomon_metric_t m;
             for(unsigned int i = 1; i < max_keys; i++) {
               for(unsigned int j = 0; j < max_val_size; j++)
+                fprintf(stderr, "Global candidate max value: %s is %lf\n", metric_name.c_str(), val_doubles[i][j]);
                 max = (max < val_doubles[i][j] ? val_doubles[i][j] : max);
             }
             metric_name += "MAX";
